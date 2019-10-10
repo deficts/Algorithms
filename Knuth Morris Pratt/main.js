e27e5909d912f1f655f7873566491518dddfabfd
deficts <diegoar7@live.com>
2019-10-10 11:11:40 -0500

    changing text to show

--- Knuth Morris Pratt/main.js
+++ Knuth Morris Pratt/main.js
@@ -3,18 +3,7 @@ $("#btnIngresar").click(function() {
   var texto = $("#texto").val();
   var ins = [];
   $.getScript("kmp.js", function() {
-    console.log(kmp(patron, texto))
     ins = kmp(patron, texto);
-    final="";
-    console.log(ins.length)
-    if(ins.length>1){
-      final=texto.substring(0,ins[1]);
-      console.log(final)
-      for(var i = 0;i<ins.length;i++){
-        final+= "<u>"+texto.substring(i,i+ins[0])+"</u>"
-      }
-    }
-    console.log(final)
-    $("#instancia").text(final);
+    $("#instancia").text(texto);
   });
 });
