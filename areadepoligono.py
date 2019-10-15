67b382e355a43b0816db33ce986e5b132c5926d2
deficts <diegoar7@live.com>
2019-10-15 09:26:45 -0500

    error at the module

--- areadepoligono.py
+++ areadepoligono.py
@@ -4,7 +4,7 @@ def area(poligono):
     a=0
     n=len(poligono)
     for i in range(n-1):
-        a+=(poligono[i].x*poligono[i+1%n].y)-(poligono[i].y*poligono[i+1%n].x)
+        a+=(poligono[i].x*poligono[(i+1)%n].y)-(poligono[i].y*poligono[(i+1)%n].x)
     return float(a/2)
 
 poligono=[Point(0,0),Point(4,0),Point(1,1),Point(0,3)]
