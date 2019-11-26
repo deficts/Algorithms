6416455dccac5bd21ed77639dd397eff742cfd1f
santiagoaragon <santiagoas77@gmail.com>
2019-11-25 19:00:06 -0600

    Se agrego el archivo.txt del genoma y se modifico el kmp.js para leer archivos.txt

--- Knuth Morris Pratt/kmp.js
+++ Knuth Morris Pratt/kmp.js
@@ -1,60 +1,88 @@
 function crearTabla(pat) {
-  const tabla = [0];
-  const m = pat.length;
-  var len = 0;
-  let i = 1;
+    const tabla = [0];
+    const m = pat.length;
+    var len = 0;
+    let i = 1;
 
-  while (i < m) {
-    if (pat[i] === pat[len]) {
-      len++;
-      tabla[i] = len;
-      i++;
-    } else {
-      if (len != 0) {
-        len = tabla[len - 1];
-      } else {
-        tabla[i] = 0;
-        i++;
-      }
+    while (i < m) {
+        if (pat[i] === pat[len]) {
+            len++;
+            tabla[i] = len;
+            i++;
+        } else {
+            if (len != 0) {
+                len = tabla[len - 1];
+            } else {
+                tabla[i] = 0;
+                i++;
+            }
+        }
     }
-  }
-  console.log(tabla)
-  return tabla;
+    console.log(tabla)
+    return tabla;
 }
 
 function kmp(pat, pal) {
-  if (pal.length === 0) {
-    return 0;
-  }
-
-  const tabla = crearTabla(pat);
-
-  var m = pat.length;
-  var n = pal.length;
-  let i = 0;
-  let j = 0;
-  var final =[];
-
-  while (i < n) {
-    //console.log("pat= "+pat[j])
-    //console.log("pal= "+pal[j])
-    if (pat[j] === pal[i]) {
-      i++;
-      j++;
+    if (pal.length === 0) {
+        return 0;
     }
-    if (j == m) {
-      final.push("Patron encontrado en el índice: " + (i - j).toString());
-      j = tabla[j - 1]
 
-    } else if (i < n && pat[j] != pal[i]) {
-      if (j != 0) {
-        j = tabla[j - 1];
+    const tabla = crearTabla(pat);
 
-      } else {
-        i++
-      }
+    var m = pat.length;
+    var n = pal.length;
+    let i = 0;
+    let j = 0;
+    var final = [];
+
+    while (i < n) {
+        //console.log("pat= "+pat[j])
+        //console.log("pal= "+pal[j])
+        if (pat[j] === pal[i]) {
+            i++;
+            j++;
+        }
+        if (j == m) {
+            final.push("Patron encontrado en el índice: " + (i - j).toString());
+            j = tabla[j - 1]
+
+        } else if (i < n && pat[j] != pal[i]) {
+            if (j != 0) {
+                j = tabla[j - 1];
+
+            } else {
+                i++
+            }
+        }
     }
-  }
-  return final;
+    return final;
 }
-kmp('aa','holacomoestaas')
+
+
+var pat1 = document.getElementById("patron1");
+var pat2 = document.getElementById("patron2");
+var pat3 = document.getElementById("patron3");
+
+
+pat1.addEventListener('click', function(){
+    loadTxt(pat1.value);
+});
+pat2.addEventListener('click', function(){
+    loadTxt(pat2.value);
+});
+pat3.addEventListener('click', function(){
+    loadTxt(pat3.value);
+});
+
+
+
+
+function loadTxt(patron) {
+    fetch('archivo.txt')
+        .then(function (response) {
+            return response.text();
+        })
+        .then(function (data) {
+            console.log(kmp(patron, data));
+        })
+}
--- Knuth Morris Pratt/main.js
+++ Knuth Morris Pratt/main.js
@@ -1,11 +1,15 @@
-$("#btnIngresar").click(function() {
-  var patron = $("#patron").val();
-  var texto = $("#texto").val();
-  var ins = [];
-  $.getScript("kmp.js", function() {
-    ins = kmp(patron, texto);
-    $("#resultado").text(texto);
-    $("#instancia").text(ins.join(" - "));
+$("#btnIngresar").click(function () {
+    var patron = $("#patron").val();
+    var texto = $("#texto").val();
+    var ins = [];
+    $.getScript("kmp.js", function () {
+        ins = kmp(patron, texto);
+        $("#resultado").text(texto);
+        $("#instancia").text(ins.join(" - "));
 
-  });
+    });
 });
+
+
+
+
